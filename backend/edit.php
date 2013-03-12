9d32511747302642439e2327c2593963f449aa0e
Albert Xing <albert.xing@alumni.ubc.ca>
2013-03-12 11:16:29 -0700

    Post Implementation
    Implement POST method for added security. Fix apostrophe in title and
    content bug (use URL encoding and HTML entities).

--- backend/edit.php
+++ backend/edit.php
@@ -1,37 +1,27 @@
 <!DOCTYPE html>
 <?php
-$page = $_GET["page"];
+$page = $_POST["page"];
 function gc($name) {
-	return file_get_contents("../meta/pages/".strtolower($name).".txt");
+	return file_get_contents("../meta/pages/".strtolower(urldecode($name)).".txt");
 }
 $json = json_decode(file_get_contents("../meta/meta.txt"), true);
 ?>
 <html>
 <head>
 	<title>Backend - <?php echo $json['name']; ?></title>
-	<link rel="stylesheet" href="css/core.min.css" type="text/css">
+	<link rel="stylesheet" href="css/core.css" type="text/css">
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<header>
-		<a href="./" title="Dashboard"><span id="logo"><?php echo $json['name']; ?></span></a>
-		<nav>
-			<ul>
-				<li><a href="./#pages">Pages</a></li>
-				<li><a href="./#new">New</a></li>
-				<li><a href="./#meta">Settings</a></li>
-			</ul>
-		</nav>
-	</header>
+	<?php include "./sidebar.php" ?>
 	<div id="frame">
 		<section id="edit">
-			<h1>Editing: <?php echo $page; ?></h1>
-			<form id="compose" action="page.php" method="GET">
-				<input type="hidden" name="titlebefore" value='<?php echo ucfirst($page); ?>'/>
-				<input type="text" placeholder="Name your page." name="title" id="title" value='<?php echo ucfirst($page); ?>'>
+			<h1>Editing: <?php echo urldecode($page); ?></h1>
+			<form id="compose" action="page.php" method="POST">
+				<input type="hidden" name="titlebefore" value='<?php echo $page; ?>'/>
+				<input type="text" placeholder="Name your page." name="title" id="title" value="<?php echo ucfirst(htmlentities(urldecode($page))); ?>">
 				<textarea name="content" id="content" rows="10" placeholder="Whatever you want to say. HTML accepted."><?php echo gc($page); ?></textarea>
 				<button type="submit" id="publish" class="button">Publish</button>
-				<a href='../<?php echo $page; ?>' class="button">Visit page</a>
 				<button name="delete" value="Delete" class="button delete" type="submit">Delete</button>
 			</form>
 		</section>
--- backend/glob.php
+++ backend/glob.php
@@ -1,21 +1,22 @@
-<?php
-function fname($name) {
-	$n = ucfirst(str_replace(".html","",substr($name,3)));
-	return (string)$n;
-};
-$pages = glob('../*.htm*');
-$nav = '';
-if (!$pages) {
-	echo 'You don\'t have any pages. Why not get started below?';
-} else {
-	foreach ($pages as $i) {
-		$name = fname($i);
-		if ($gedit==true) {
-			echo '<a href="./edit.php?page='.strtolower($name).'" class="page"><li class="button page">' . $name . '</li></a>';
-		}
-		else {
-			$nav = $nav . '<li><a href="./'.strtolower($name).'" class="page">' . $name . '</a></li>';
+<ul>
+	<?php
+	function fname($name) {
+		$n = ucfirst(str_replace(".html","",substr($name,3)));
+		return (string)$n;
+	};
+	$pages = glob('../*.htm*');
+	if (!$pages) {
+		echo 'You don\'t have any pages. Why not get started below?';
+	} else {
+		foreach ($pages as $i) {
+			$name = fname($i);
+			if ($gedit==true) {
+				echo '<form action="./edit.php" method="POST"><button type="submit" name="page" value="' . rawurlencode($name). '" class="page button">' . $name . '</button></form>';
+			}
+			else {
+				echo '<li><a href="../'.rawurlencode($name).'" class="page">' . $name . '</a></li>';
+			}
 		}
 	}
-}
-?>
+	?>
+</ul>
