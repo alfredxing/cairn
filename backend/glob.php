e4b1161bd51a302acf914f8ee47af0fa25f84af5
Alfred Xing <xing@lfred.info>
2013-03-12 12:22:07 -0700

    Refresh function
    Reappend refresh function to dashboard and glob.

--- backend/glob.php
+++ backend/glob.php
@@ -5,6 +5,7 @@
 		return (string)$n;
 	};
 	$pages = glob('../*.htm*');
+	$nav = '';
 	if (!$pages) {
 		echo 'You don\'t have any pages. Why not get started below?';
 	} else {
@@ -12,6 +13,9 @@
 			$name = fname($i);
 			if ($gedit==true) {
 				echo '<form action="./edit.php" method="POST"><button type="submit" name="page" value="' . rawurlencode($name). '" class="page button">' . $name . '</button></form>';
+			}
+			elseif ($refresh==true) {
+				$nav = $nav . '<li><a href="./'.rawurlencode($name).'" class="page">' . $name . '</a></li>';
 			}
 			else {
 				echo '<li><a href="../'.rawurlencode($name).'" class="page">' . $name . '</a></li>';
