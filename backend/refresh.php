aff19f4ff42516c04223f65d2a62594139a4d92b
Alfred Xing <xing@lfred.info>
2013-03-12 12:23:46 -0700

    Update refresh
    Update refresh page.

--- backend/refresh.php
+++ backend/refresh.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
+<?php
+$json = json_decode(file_get_contents("../meta/meta.txt"), true);
+?>
 <html>
 <head>
-	<title>Dashboard: The Backend - Cairn</title>
+	<title>Backend - <?php echo $json['name']; ?></title>
 	<link rel="stylesheet" href="css/core.css" type="text/css">
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 </head>
